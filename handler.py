c3ef840094d5770cb37a93d589595b57283dbec9
16682 <102009548+16682@users.noreply.github.com>
2026-05-13 07:29:02 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -397,7 +397,7 @@ class MainHandler:
             defaultConfig = {
                 'remarkName': '默认备注名',
                 'model': 'Mi 13',
-                'appVersion': '9.5.0',
+                'appVersion': '9.9.99',
                 'systemVersion': '13',
                 'systemName': 'android',
                 "signVersion": "first_v3",
